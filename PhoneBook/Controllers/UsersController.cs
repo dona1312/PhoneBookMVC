de1eb529a32894b84365629848884ce105c8b6f4
Dona Vasileva <Dona Vasileva>
2016-04-13 12:50:09 +0300

    Edit List Views-add Search button

--- PhoneBook/Controllers/UsersController.cs
+++ PhoneBook/Controllers/UsersController.cs
@@ -7,9 +7,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PhoneBook.Filters;
 
 namespace PhoneBook.Controllers
 {
+    [AuthenticationFilter]
     public class UsersController : Controller
     {
 
